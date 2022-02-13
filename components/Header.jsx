11b92fe4420ed7b404535e1339d9e9e1a2691077
Kim-Jeongyoung <jeongyoung2@gmail.com>
2022-02-13 14:16:48 -0800

    Add logo svg

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,9 +2,23 @@ import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import { styled } from '@mui/system';
+
+//A home icon logo
 import IconButton from '@mui/material/IconButton';
 import Home from '@mui/icons-material/Home';
 import MuiNextLink from '@components/MuiNextLink';
+
+// logo test
+// import Icon from '@material-ui/core/Icon
+// import { ReactComponent as Logo } from '../public/rollin-logo-white.svg';
+// import Logo from '../public/rollin-logo-dark.svg';
+// import ConcretIcon from './ConcretIcon';
+// 나래님 방법
+// import { ReactComponent as SushiLogo } from '../assets/rollin-logo-dark.svg';
+//Johnny https://youtu.be/9tJTEGG0t-M
+// https://www.npmjs.com/package/@svgr/webpack
+import Logo from 'components/svgs/rollin-logo-white.svg';
+
 import Navbar from './Navbar';
 import SideDrawer from './SideDrawer';
 import HideOnScroll from './HideOnScroll';
@@ -18,7 +32,6 @@ export const navLinks = [
   { title: `home`, path: `/` },
   { title: `about us`, path: `/about-us` },
   { title: `menu`, path: `/menu` },
-
   { title: `catering`, path: `/catering` },
   { title: `contact`, path: `/contact` },
 ];
@@ -28,6 +41,7 @@ const Header = () => {
     <>
       <HideOnScroll>
         <AppBar position="fixed">
+          {/* <AppBar position="fixed" color="inherit"> */}
           <Toolbar>
             <Container
               maxWidth="lg"
@@ -35,12 +49,16 @@ const Header = () => {
             >
               <IconButton edge="start" aria-label="home">
                 <MuiNextLink activeClassName="active" href="/">
-                  <Home
+                  {/* <Home
                     sx={{
                       color: (theme) => theme.palette.common.white,
                     }}
                     fontSize="large"
-                  />
+                  /> */}
+
+                  {/* <SushiLogo /> */}
+
+                  <Logo />
                 </MuiNextLink>
               </IconButton>
 
--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: '@svgr/webpack', option: { icon: true } }],
+    });
+    return config;
+  },
+};
+
+// https://youtu.be/9tJTEGG0t-M
